3e04c4207da508475ce686c185ad7b4268b93e42
Tallys Witer <tallyswiter@hotmail.com>
2025-06-16 19:35:38 -0300

    Criação da camada de dados da aplicação
    Criação da camada de dados da aplicação:
    
    * Listar, adicionar, atualizar e remover dados;
    * Criação do database com as bibliotecas floor e sqflite;

---
+++ lib/data/dao/entry_dao.dart
@@ -0,0 +1,17 @@
+import 'package:floor/floor.dart';
+import 'package:hyrule/domain/models/entry.dart';
+
+@dao
+abstract class EntryDao {
+  @Query('SELECT * FROM ENTRY')
+  Future<List<Entry>> getAllEntries();
+
+  @Query('SELECT * FROM ENTRY WHERE id = :id')
+  Future<Entry?> getEntryById(int id);
+
+  @Insert(onConflict: OnConflictStrategy.replace)
+  Future<void> addEntry(Entry entry);
+
+  @delete
+  Future<void> removeEntry(Entry entry);
+}
